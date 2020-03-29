1f6cfc5fc8f43f4f35b52907b61cba75f969dfea
Hongyu Liu <hongyu.liu@mail.utoronto.ca>
2020-03-29 12:30:57 -0400

    dynamodb setup

--- app/events.py
+++ app/events.py
@@ -6,60 +6,64 @@ from flask import (Blueprint, flash, redirect, render_template, request,
                    session, url_for)
 import app
 
+
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+dynamodb_client = boto3.client('dynamodb', region_name='us-east-1')
+table = dynamodb.Table('Events')
+# table_client = dynamodb_client.table('Events')
 bp = Blueprint("events", __name__, url_prefix='/events')
 
 
 @bp.route('/', methods=['GET', 'POST'])
 def events():
-    table = app.dynamodb.Table('Events')
+    if request.method == "POST":
+        response = table.put_item(
+            Item={
+                'username': 'eric',
+                'date': 1585462294,  # timestamp
+                'title': 'tennis',
+                'required_parti_num': 10,
+                'address': 'st george',
+                'is_active': 1,
+                'item_type': 'host',
+                'endtime': 100000, #seconds
+                'event_type': "sports"
+            }
+        )
+        return response
 
-    response = table.put_item(
-        Item={
-            'username': 'eric',
-            'date': '1585462294',  # timestamp
-            'title': 'tennis',
-            'required_parti_num': 10,
-            'address': 'st george',
-            'is_active': 1,
-            'stars': 5,
-            'type': 'host'
-        }
-    )
     # flash('Registration Success! Please login.', 'success')
 
 
 @bp.route('/join', methods=['POST'])
 def join_event():
-    table = app.dynamodb.Table('Events')
-    response = table.put_item(
-        Item={
-            'username': 'eric',
-            'date': '1585462294',  # timestamp
-            'title': 'tennis',
-            'type': 'participant'
-        })
+    if request.method == "POST":
 
-    return 'join event'
+        response = table.put_item(
+            Item={
+                'username': 'sara',
+                'date': 158546224,  # current timestamp
+                'title': 'tennis',
+                'item_type': 'participant'
+            })
+
+        return response
 
 
 @bp.route('/rate', methods=['POST'])
 def rate_event():
-    table = app.dynamodb.Table('Events')
     response = table.update_item(
         Key={
-            'username': 'eric',
-            'date': '1585462294',  # timestamp
-            'title': 'tennis',
-            'type': 'participant'
+            'username': 'sara',
+            'date': 158546224,  # current timestamp
         },
-        UpdateExpression="set star = :r",
+        UpdateExpression="SET stars= :var1",
         ExpressionAttributeValues={
-            ':r': 3,
-        },
+                ':var1': 1
+                },
         ReturnValues="UPDATED_NEW"
-    )
 
+            )
     return 'rate events'
 
 # if __name__== "__main__":
