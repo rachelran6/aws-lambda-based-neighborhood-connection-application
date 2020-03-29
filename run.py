671fa7d94cd5158b3e1d8268f921550c63822fdd
Hongyu Liu <hongyu.liu@mail.utoronto.ca>
2020-03-29 02:24:53 -0400

    create database Event

--- run.py
+++ run.py
@@ -1,1 +1,11 @@
 from app import webapp
+# import logging
+from app import create_app
+
+
+user_app = create_app()
+
+# logging.basicConfig(filename='request.log', level=logging.CRITICAL)
+
+if __name__ == "__main__":
+    user_app.run(debug=True)
